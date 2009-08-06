438a9a6943780e2e2bbbe98f0835bac309e98ba8
cedric <cedric@98cce944-dcf5-11de-9279-77a91f40074c>
2009-08-06 20:18:33 +0000

    * Mls: todo's

--- clp/mls/Mls.lua
+++ clp/mls/Mls.lua
@@ -12,7 +12,6 @@
 --       when pressed Back), tiles-engine-evolution (for this one, some 
 --       ScrollMap methods are missing, see ML source/libs to see which ones)
 --       alternativ-keyboard is dog-slow, too
--- @todo Activate mirrorH/V, in the same buggy way as ML
 -- @todo INI, Map, Mod, Sound, Sprite, ScrollMap: raise an error when a file is
 --       not found ? Check what ML does when it happens. I've already done this
 --       for Image.load()
