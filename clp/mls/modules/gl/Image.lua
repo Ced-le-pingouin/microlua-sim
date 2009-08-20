fd5335c52c263f8867f27392a7c6cf57be0f2d58
cedric <cedric@98cce944-dcf5-11de-9279-77a91f40074c>
2009-08-20 21:38:53 +0000

    * implémentation de Image.setTint() en OpenGL

--- clp/mls/modules/gl/Image.lua
+++ clp/mls/modules/gl/Image.lua
@@ -60,7 +60,7 @@ function M.load(path, destination)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE)
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE)
     -- since OpenGL textures have their 0,0 origin at the bottom left, we need
     -- to flip the loaded texture around the Y axis, so we can use the ML 0,0 
     -- origin that is at the TOP left
