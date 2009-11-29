3fcb2c7fd75278926b3ab1ff53ad734a78b0149c
cedric <cedric@98cce944-dcf5-11de-9279-77a91f40074c>
2009-11-29 09:16:20 +0000

    * commentaires

--- clp/mls/Mls.lua
+++ clp/mls/Mls.lua
@@ -5,7 +5,7 @@
 -- @name clp.mls.Mls
 -- @author Ced-le-pingouin <Ced.le.pingouin@gmail.com>
 --
--- @todo The spr_depl script (maybe other?) sometimes crashes suddenly, often 
+-- @todo The spr_depl script (maybe others?) sometimes crashes suddenly, often 
 --       after other scripts having been loaded before. I'm suspecting the many 
 --       Images needed for Font cache. Maybe some <module>:resetModule() should 
 --       dispose of resources (and thus keep track of the resources they load
