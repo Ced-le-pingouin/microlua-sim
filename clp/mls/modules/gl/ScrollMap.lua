60671c7a34ee0c63ddaa810e275575c3e67148f0
Ced.le.pingouin <Ced.le.pingouin@98cce944-dcf5-11de-9279-77a91f40074c>
2011-03-29 10:18:45 +0000

    ScrollMap (gl) now uses OpenGL display lists, should be faster but READ MORE.
    When the scrollmap is loaded, a display list to display it as a whole is
    created. So, when draw() is called, we only need to call the display list once
    (or more because the scrollmap must be repeated), which is very fast.
    
    On the other hand, like we do in ScrollMap (wx), whenever a tile is changed 
    with setTile() , the display list needs to be recompiled. The script 
    KnightmareDS uses setTile(), for example.
    
    The current version is good enough for now, but maybe later we'll need to 
    speed things up for cases when setTile() is used often.
    
    Possible solutions to investigate then:
      . having one display list per row or per column, and only change the display
        list affected by setTile() when needed
      . having one diplay list per *tile* in the image, and use glCallLists()
        (notice the plural form) to display them in the right order. When 
        setTile() is called, we only need to change the "array" we pass to 
        glCallLists(). Not sure if this will be really fast, or if I even 
        understood correctly how glCallLists is supposed to work... :)

--- clp/mls/modules/gl/ScrollMap.lua
+++ clp/mls/modules/gl/ScrollMap.lua
@@ -23,20 +23,139 @@
 --  You should have received a copy of the GNU General Public License
 --  along with Micro Lua DS Simulator.  If not, see <http://www.gnu.org/licenses/>.
 
+require "luagl"
 local Class = require "clp.Class"
 local Map = require "clp.mls.modules.Map"
 
 local M = Class.new(Map)
 
-function M.draw(screenNum, scrollmap)
-    screen.setClippingForScreen(screenNum)
+function M.new(image, mapfile, width, height, tileWidth, tileHeight)
+    local scrollmap = M.super().new(image, mapfile, width, height, 
+                                    tileWidth, tileHeight)
     
-    M.super().draw(screenNum, scrollmap, 0, 0, 
-                   scrollmap._width, scrollmap._height, true, true)
+    scrollmap._displayList = glGenLists(1)
+    assert(scrollmap._displayList ~= 0, "ERROR: can't allocate OpenGL display list for ScrollMap")
+    
+    scrollmap._totalWidth = width * tileWidth
+    scrollmap._totalHeight = height * tileHeight
+    
+    scrollmap._tilesHaveChanged = true
+    
+    M._compileDisplayList(scrollmap)
+    
+    return scrollmap
 end
 
-function M.space(map, x, y)
+function M.destroy(scrollmap)
+    glDeleteLists(scrollmap._displayList, 1)
+    
+    M.super().destroy(scrollmap)
+end
+
+function M.draw(screenNum, scrollmap)
+    local posX, posY = -scrollmap._scrollX, -scrollmap._scrollY
+    local width  = scrollmap._totalWidth
+    local height = scrollmap._totalHeight
+    
+    while posX > 0 do posX = posX - width end
+    while posY > 0 do posY = posY - height end
+    
+    local startPosX = posX
+    
+    if scrollmap._tilesHaveChanged then
+        M._compileDisplayList(scrollmap)
+    end
+    
+    screen.setClippingForScreen(screenNum)
+    
+    while posY < SCREEN_HEIGHT do
+        while posX < SCREEN_WIDTH do
+            glPushMatrix()
+                glTranslated(posX, screen.offset[screenNum] + posY, 0)
+                glCallList(scrollmap._displayList)
+            glPopMatrix()
+            
+            posX = posX + width
+        end
+        posY = posY + height
+        posX = startPosX
+    end
+end
+
+function M.space(scrollmap, x, y)
     error("ScrollMap doesn't support space()")
+end
+
+function M.setTile(scrollmap, x, y, tile)
+    scrollmap._data[y][x] = tile
+    
+    scrollmap._tilesHaveChanged = true
+end
+
+function M._compileDisplayList(scrollmap)
+    local image = scrollmap._tilesImage
+    local tilesPerRow = scrollmap._tilesPerRow
+    local tileWidth, tileHeight = scrollmap._tileWidth, scrollmap._tileHeight
+    
+    local xRatio, yRatio
+    if M.normalizeTextureCoordinates then
+        xRatio, yRatio = 1 / image._textureWidth, 1 / image._textureHeight
+    end
+    
+    glNewList(scrollmap._displayList, GL_COMPILE)
+        glEnable(screen.textureType)
+        glBindTexture(screen.textureType, image._textureId[0])
+        
+        local tint = image._tint
+        local r, g, b = tint:Red() / 255, tint:Green() / 255, tint:Blue() / 255
+        glColor3d(r, g, b)
+        
+        glBegin(GL_QUADS)
+        
+        local lastRow, lastCol = scrollmap._height - 1, scrollmap._width - 1
+        local posY = 0
+        for row = 0, lastRow do
+            local posX = 0
+            
+            for col = 0, lastCol do
+                local tileNum = scrollmap._data[row][col]
+                local sourcex = (tileNum % tilesPerRow) * tileWidth
+                local sourcey = math.floor(tileNum / tilesPerRow) * tileHeight
+                
+                local sourcex2 = sourcex + tileWidth - 0.01
+                local sourcey2 = sourcey + tileHeight - 0.01
+                sourcex = sourcex + 0.01
+                sourcey = sourcey + 0.01
+                
+                if M.normalizeTextureCoordinates then
+                    sourcex = sourcex * xRatio
+                    sourcey = sourcey * yRatio
+                    sourcex2 = sourcex2 * xRatio
+                    sourcey2 = sourcey2 * yRatio
+                end
+                
+                glTexCoord2d(sourcex, sourcey)
+                glVertex2d(posX, posY)
+                
+                glTexCoord2d(sourcex2, sourcey)
+                glVertex2d(posX + tileWidth, posY)
+                
+                glTexCoord2d(sourcex2, sourcey2)
+                glVertex2d(posX + tileWidth, posY + tileHeight)
+                
+                glTexCoord2d(sourcex, sourcey2)
+                glVertex2d(posX, posY + tileHeight)
+                
+                posX = posX + tileWidth
+            end
+            
+            posY = posY + tileHeight
+        end
+        
+        glEnd()
+    glEndList()
+    
+    scrollmap._tilesHaveChanged = false
 end
 
 return M
